b640d2db7196828de923e3fc676ffad15adaf40c
Tony Edgin <tedgin@cyverse.org>
2024-01-11 15:37:15 -0700

    Dealt with pylint issues and documented vectordb.py

--- propeller/rag/vectordb.py
+++ propeller/rag/vectordb.py
@@ -1,16 +1,20 @@
-from typing import Optional, List, Literal
-from langchain_core.documents import Document
-from langchain_core.vectorstores import VectorStoreRetriever
+# -*- coding: <encoding name> -*-
+
+"""This module holds the vector database maintenance logic."""
+
+import pathlib
+from typing import Optional, Literal
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import GPT4AllEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma   # pylint: disable=no-name-in-module
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain_community.document_loaders import UnstructuredPowerPointLoader
 from langchain_community.document_loaders import TextLoader
+from langchain_core.vectorstores import VectorStoreRetriever
 
 import tiktoken
-import pathlib
 
 def _tiktoken_len(text):
     tokenizer = tiktoken.get_encoding("cl100k_base")
@@ -18,16 +22,25 @@ def _tiktoken_len(text):
     return len(tokens)
 
 class VectorDB:
+    """
+    This class manages the maintenance of a vector database, e.g., adding
+    documents. When constructing one, the path to the folder where the database
+    will be persisted should be provided. If it isn't the database will not be
+    persisted.
+    """
 
     def __init__(self, db_path:Optional[str]):
         self._db_path = db_path
         self._impl = Chroma(embedding_function=GPT4AllEmbeddings(), persist_directory=db_path)
 
-    def add_docs(self, docs:List[Document]) -> None:
-        self._impl = Chroma.from_documents(
-            documents=docs, embedding=self._impl.embeddings, persist_directory=self._db_path)
-
     def add_file(self, path:str) -> None:
+        """
+        Adds a file to the vector store. It will use the file's extension to
+        determine the type of file.
+
+        Params:
+          path  The path to the file on the local filesystem
+        """
         # detect format
         file_ext = pathlib.Path(path).suffix
         match str.lower(file_ext):
@@ -41,15 +54,39 @@ class VectorDB:
                 self.add_text_file(path)
 
     def add_markdown(self, markdown_path:str) -> None:
-        self.add_docs(UnstructuredMarkdownLoader(markdown_path).load())
+        """
+        Adds a markdown file to the vector store.
+
+        Params:
+          path  The path to the file on the local filesystem
+        """
+        self._add_docs(UnstructuredMarkdownLoader(markdown_path).load())
 
     def add_pdf(self, pdf_path:str) -> None:
-        self.add_docs(PyPDFLoader(pdf_path).load_and_split())
+        """
+        Adds a PDF file to the vector store.
+
+        Params:
+          path  The path to the file on the local filesystem
+        """
+        self._add_docs(PyPDFLoader(pdf_path).load_and_split())
 
     def add_pptx(self, pptx_path:str) -> None:
-        self.add_docs(UnstructuredPowerPointLoader(pptx_path).load())
+        """
+        Adds a PowerPoint file to the vector store.
+
+        Params:
+          path  The path to the file on the local filesystem
+        """
+        self._add_docs(UnstructuredPowerPointLoader(pptx_path).load())
 
     def add_text(self, text:Literal) -> None:
+        """
+        Adds a block of text to the vector store.
+
+        Params:
+          text  the block of text
+        """
         # this splits the input text
         text_splitter = RecursiveCharacterTextSplitter(
             # chunk size should not be very large as model has a limit
@@ -59,10 +96,21 @@ class VectorDB:
             length_function = _tiktoken_len,
         )
         # chunk input into multiple documents
-        self.add_docs(text_splitter.create_documents([text]))
+        self._add_docs(text_splitter.create_documents([text]))
 
     def add_text_file(self, text_path:str) -> None:
-        self.add_docs(TextLoader(text_path).load())
+        """
+        Adds a text file of unknown type to the vector store.
+
+        Params:
+          path  The path to the file on the local filesystem
+        """
+        self._add_docs(TextLoader(text_path).load())
+
+    def _add_docs(self, docs):
+        self._impl = Chroma.from_documents(
+            documents=docs, embedding=self._impl.embeddings, persist_directory=self._db_path)
 
     def as_retriever(self) -> VectorStoreRetriever:
+        """Return VectorStoreRetriever initialized from this VectorStore."""
         return self._impl.as_retriever()
