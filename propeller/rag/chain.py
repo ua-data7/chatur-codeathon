ad84d29b4ac20fa12cd0647e103d6803a9564e59
Illyoung Choi <iychoi@email.arizona.edu>
2024-01-30 16:17:03 -0700

    use conversational retrieval chain

--- propeller/rag/chain.py
+++ propeller/rag/chain.py
@@ -1,4 +1,8 @@
 
+from langchain.chains import (
+    LLMChain,
+    ConversationalRetrievalChain
+)
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import (
     ChatPromptTemplate,
@@ -9,13 +13,10 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_community.llms import Ollama
 
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import (
+    ConversationBufferMemory
+)
 
-from langchain_core.runnables import RunnablePassthrough
-
-
-def format_documents(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
 
 # Prompt
 prompt = ChatPromptTemplate(
@@ -24,7 +25,8 @@ prompt = ChatPromptTemplate(
             "Using the following documents, help answer questions as a teacher would help a student. Remember to only answer the question they asked: {context}"
         ),
         HumanMessagePromptTemplate.from_template("{question}"),
-    ]
+    ],
+    output_parser=StrOutputParser()
 )
 
 # LLM
@@ -33,12 +35,13 @@ llm = Ollama(
     callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 # question from human
 def make_chain(retriever):
-    return (
-        {"context": retriever | format_documents, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
+    return ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        memory=memory,
+        combine_docs_chain_kwargs={"prompt": prompt}
     )
